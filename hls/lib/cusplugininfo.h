022e52f884fecf0d1bdfa14f2cf556940e47c426
yjjnls <x-jj@foxmail.com>
2018-07-16 02:58:16 -0700

    cmake install location change for linux build

--- hls/lib/cusplugininfo.h
+++ hls/lib/cusplugininfo.h
@@ -1,7 +1,7 @@
 #ifndef _CUSTOM_PLUGIN_INFO_H_
 #define _CUSTOM_PLUGIN_INFO_H_
 
-#define VERSION "1.14.1"
+#define VERSION "1.14.0"
 #define GST_LICENSE "LGPL"
 #define PACKAGE "GStreamer Bad Plug-ins"
 #define PACKAGE_NAME "custom hls"
