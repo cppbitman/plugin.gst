1f91ef44604982bc10f3ffceb48043ef5a8c309e
yjjnls <x-jj@foxmail.com>
2018-08-13 17:23:50 +0800

    build redefiniton

--- memorysink/lib/packageinfo.h
+++ memorysink/lib/packageinfo.h
@@ -3,7 +3,9 @@
 
 #include "../../version.h"
 #define PACKAGE "GStreamer Custom Plug-ins"
+
 #define PACKAGE_NAME "custom plugins"
+
 #define GST_PACKAGE_ORIGIN "https://gstreamer.freedesktop.org/src"
 #define GST_PACKAGE_NAME PACKAGE_NAME
 #endif
